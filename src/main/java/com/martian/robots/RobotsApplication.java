3327626d1f6283599326785aa847c98061aa334e
rajeshgubba1984 <rajeshgubba1984@gmail.com>
2021-12-08 20:00:03 +0000

    Initial project structure

---
+++ src/main/java/com/martian/robots/RobotsApplication.java
@@ -0,0 +1,13 @@
+package com.martian.robots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martian/robots/robots/RobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martian.robots.robots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
